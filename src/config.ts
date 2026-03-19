1c870decc090ab13b82334a4b3a3cf2e64569eb5
suhcodes <contact@suuh.codes>
2026-03-19 14:43:00 -0300

    updates blog title

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export const SITE = {
-  website: "https://suhcodes.netlify.app/", // replace with your deployed domain
-  author: "Suh",
-  profile: "https://www.linkedin.com/in/suh-moraes",
-  desc: "A dev blog documenting experiments, builds, and field notes.",
-  title: "Suh Codes",
-  ogImage: "public/mylogo.png", // add your OG image to public/ and reference it here
+  website: 'https://suhcodes.netlify.app/', // replace with your deployed domain
+  author: 'Suh',
+  profile: 'https://www.linkedin.com/in/suh-moraes',
+  desc: 'A dev blog documenting experiments, builds, and field notes.',
+  title: 'suh.codes',
+  ogImage: 'public/mylogo.png', // add your OG image to public/ and reference it here
   lightAndDarkMode: true,
   postPerIndex: 4,
   postPerPage: 4,
@@ -13,11 +13,11 @@ export const SITE = {
   showBackButton: true,
   editPost: {
     enabled: false,
-    text: "Edit page",
-    url: "",
+    text: 'Edit page',
+    url: '',
   },
   dynamicOgImage: true,
-  dir: "ltr",
-  lang: "en",
-  timezone: "America/Sao_Paulo", // update to your timezone
-} as const;
+  dir: 'ltr',
+  lang: 'en',
+  timezone: 'America/Sao_Paulo', // update to your timezone
+} as const
